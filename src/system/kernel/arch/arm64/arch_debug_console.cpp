585b9dc496f80761b776f9e8b7b69b6f55733d56
milek7 <me@milek7.pl>
2022-03-27 22:04:49 +0200

    use hardcoded pl011 in arm64 for debug

--- src/system/kernel/arch/arm64/arch_debug_console.cpp
+++ src/system/kernel/arch/arm64/arch_debug_console.cpp
@@ -1,13 +1,25 @@
 /*
- * Copyright 2019 Haiku, Inc. All Rights Reserved.
+ * Copyright 2007, François Revol, revol@free.fr.
  * Distributed under the terms of the MIT License.
+ *
+ * Copyright 2003-2006, Axel Dörfler, axeld@pinc-software.de.
+ * Distributed under the terms of the MIT License.
+ *
+ * Copyright 2001, Travis Geiselbrecht. All rights reserved.
+ * Distributed under the terms of the NewOS License.
  */
+
+
 #include <arch/debug_console.h>
 #include <arch/generic/debug_uart.h>
+#include <arch/arm/arch_uart_pl011.h>
 #include <boot/kernel_args.h>
 #include <kernel.h>
 #include <vm/vm.h>
 #include <string.h>
+
+
+static DebugUART *sArchDebugUART = NULL;
 
 
 void
@@ -25,34 +37,43 @@ arch_debug_install_interrupt_handlers(void)
 int
 arch_debug_blue_screen_try_getchar(void)
 {
-	return -1;
+	// TODO: Implement correctly!
+	return arch_debug_blue_screen_getchar();
 }
 
 
 char
 arch_debug_blue_screen_getchar(void)
 {
-	return -1;
+	return arch_debug_serial_getchar();
 }
 
 
 int
 arch_debug_serial_try_getchar(void)
 {
-	return -1;
+	// TODO: Implement correctly!
+	return arch_debug_serial_getchar();
 }
 
 
 char
 arch_debug_serial_getchar(void)
 {
-	return -1;
+	if (sArchDebugUART == NULL)
+		return '\0';
+sArchDebugUART->PutChar('x');
+	return sArchDebugUART->GetChar(false);
 }
 
 
 void
 arch_debug_serial_putchar(const char c)
 {
+	if (sArchDebugUART == NULL)
+		return;
+
+	sArchDebugUART->PutChar(c);
 }
 
 
@@ -74,12 +95,23 @@ arch_debug_serial_puts(const char *s)
 void
 arch_debug_serial_early_boot_message(const char *string)
 {
+	arch_debug_serial_puts(string);
 }
 
 
 status_t
 arch_debug_console_init(kernel_args *args)
 {
+	// As a last try, lets assume qemu's pl011 at a sane address
+	if (sArchDebugUART == NULL)
+		sArchDebugUART = arch_get_uart_pl011(KERNEL_PMAP_BASE + 0x9000000, 0x16e3600);
+
+	// Oh well.
+	if (sArchDebugUART == NULL)
+		return B_ERROR;
+
+	sArchDebugUART->InitEarly();
+
 	return B_OK;
 }
 
