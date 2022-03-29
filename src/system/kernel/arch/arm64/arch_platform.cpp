926fbd948ad7ee7492a1b765616b12b6cc4f93f2
milek7 <me@milek7.pl>
2022-03-29 02:57:10 +0200

    wire up acpi and fdt

--- src/system/kernel/arch/arm64/arch_platform.cpp
+++ src/system/kernel/arch/arm64/arch_platform.cpp
@@ -5,7 +5,11 @@
 #include <KernelExport.h>
 #include <arch/platform.h>
 #include <boot/kernel_args.h>
+#include <arch_kernel.h>
+#include <boot_item.h>
 
+void *gFDT;
+phys_addr_t sACPIRootPointer;
 
 status_t
 arch_platform_init(struct kernel_args *kernelArgs)
@@ -15,8 +19,17 @@ arch_platform_init(struct kernel_args *kernelArgs)
 
 
 status_t
-arch_platform_init_post_vm(struct kernel_args *kernelArgs)
+arch_platform_init_post_vm(struct kernel_args *args)
 {
+	if (args->arch_args.fdt) {
+		gFDT = (void*)(KERNEL_PMAP_BASE + (uint64_t)args->arch_args.fdt.Get());
+	}
+
+	if (args->arch_args.acpi_root) {
+		sACPIRootPointer = args->arch_args.acpi_root.Get();
+		add_boot_item("ACPI_ROOT_POINTER", &sACPIRootPointer, sizeof(sACPIRootPointer));
+	}
+
 	return B_OK;
 }
 
